0f2628e6980fbdec22c95e747303eca07c8fe759
22836252 <ken99899@gmail.com>
2020-03-19 13:09:16 +0800

    new1
    new1

--- mblog/settings.py
+++ mblog/settings.py
@@ -68,9 +68,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mblog.wsgi.application'
 
 
-
-DATABASES['default'] = dj_database_url.config(default='postgres://user:[email protected]/db') 
-DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2' 
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
