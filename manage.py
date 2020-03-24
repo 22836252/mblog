030dace04563acaef790991d83012a9c40af0554
22836252 <ken99899@gmail.com>
2020-03-24 09:53:09 +0800

    update
    1

--- manage.py
+++ manage.py
@@ -6,10 +6,7 @@ import sys
 
 def main():
 
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mblog.herokusettings')
-    
-   
-    #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mblog.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mblog.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
