3ab0f11d94b53a820081549487ed8d191e3843ab
22836252 <ken99899@gmail.com>
2020-03-24 11:37:49 +0800

    update
    update

--- mblog/settings.py
+++ mblog/settings.py
@@ -67,19 +67,19 @@ WSGI_APPLICATION = 'mblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'newshop',  # 数据库名字(需要先创建)
-        'USER': 'postgres',  # 登录用户名
-        'PASSWORD': 'admin',  # 密码
-        'HOST': '',  # 数据库IP地址,留空默认为localhost
-        # 'PORT': '5432',  # 端口
-        'CONN_MAX_AGE': 500,
-        'ssl_require':True
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'newshop',  # 数据库名字(需要先创建)
+#         'USER': 'postgres',  # 登录用户名
+#         'PASSWORD': 'admin',  # 密码
+#         'HOST': '',  # 数据库IP地址,留空默认为localhost
+#         # 'PORT': '5432',  # 端口
+#         'CONN_MAX_AGE': 500,
+#         'ssl_require':True
 
-    }
-}
+#     }
+# }
 
 
 # Password validation
@@ -147,3 +147,5 @@ SESSION_SAVE_EVERY_REQUEST = False
 
 
 django_heroku.settings(locals())
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
